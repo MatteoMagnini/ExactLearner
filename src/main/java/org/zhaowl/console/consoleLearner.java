c7957f9756763cf6edabe664b0466373a93f12c4
bkonev <konev@liverpool.ac.uk>
2018-05-10 20:28:14 +0100

    number of concepts and roles moved into stats fixed

--- src/main/java/org/zhaowl/console/consoleLearner.java
+++ src/main/java/org/zhaowl/console/consoleLearner.java
@@ -123,7 +123,7 @@ public class consoleLearner {
 				long timeEnd = System.currentTimeMillis();
 				saveOWLFile(hypothesisOntology, hypoFile);
                 victory();
-                printStats(timeStart, timeEnd, args, false);
+                printStats(timeStart, timeEnd, args, true);
 				elQueryEngineForH.disposeOfReasoner();
 				elQueryEngineForT.disposeOfReasoner();
 				myManager.removeOntology(hypothesisOntology);
@@ -188,7 +188,7 @@ public class consoleLearner {
 		printStat("Total branchings: ", elOracle.getNumberBranching(), verb);
 		printStat("Total saturations: ", elOracle.getNumberSaturations(), verb);
 		printStat("Total unsaturations: ", elOracle.getNumberUnsaturations(), verb);
-		printStat("\nSizes:", verb);
+		printStat("\nOntology sizes:", verb);
 		printStat("Target TBox logical axioms: ", targetOntology.getAxiomCount(AxiomType.SUBCLASS_OF)+
 				targetOntology.getAxiomCount(AxiomType.EQUIVALENT_CLASSES), verb);
 		myMetrics.computeTargetSizes(targetOntology);
@@ -196,10 +196,10 @@ public class consoleLearner {
 		printStat("Size of T: ", myMetrics.getSizeOfTarget(), verb);
 		printStat("Hypothesis TBox logical axioms: ",  hypothesisOntology.getAxiomCount(AxiomType.SUBCLASS_OF)+
 				hypothesisOntology.getAxiomCount(AxiomType.EQUIVALENT_CLASSES), verb);
-		printStat("Number of concept names", conceptNumber, verb);
-		printStat("Number of role names", roleNumber, verb);
 		printStat("Size of H: ", myMetrics.getSizeOfHypothesis(), verb);
-		printStat("Size of largest  concept in T: ", myMetrics.getSizeOfTargetLargestConcept(), verb);
+        printStat("Number of concept names: ", conceptNumber, verb);
+        printStat("Number of role names: ", roleNumber, verb);
+        printStat("Size of largest  concept in T: ", myMetrics.getSizeOfTargetLargestConcept(), verb);
 		printStat("Size of largest  concept in H: ", myMetrics.getSizeOfHypothesisLargestConcept(), verb);
 		//printStat("Size of largest  concept (sum conjunctions) in T: ", myMetrics.getSumSizeOfLargestConcept(), verb); 
 		 
