4cf5168d760adaa8be119c1d7c3511f26947e9cd
Boris Konev <17253548+bkonev@users.noreply.github.com>
2018-06-26 14:42:33 +0100

    checking if an input ontology is a terminology

--- src/main/java/org/exactlearner/console/terminologyChecker.java
+++ src/main/java/org/exactlearner/console/terminologyChecker.java
@@ -10,7 +10,6 @@ import java.util.Set;
 public class terminologyChecker {
 
 
-
     public static void main(String[] args) {
         try {
             // targetOntology from parameters
@@ -24,29 +23,57 @@ public class terminologyChecker {
 
             Boolean terminology = true;
 
-            for (OWLAxiom axe : targetOntology.getAxioms()) {
+            for (OWLAxiom axe : targetOntology.getLogicalAxioms()) {
+
+
+
+
                 if (axe.isOfType(AxiomType.SUBCLASS_OF)) {
                     OWLSubClassOfAxiom sax = (OWLSubClassOfAxiom) axe;
                     if (sax.getSubClass().isAnonymous() && sax.getSuperClass().isAnonymous()) {
                         terminology = false;
+                        break;
                     }
-                }
-                if (axe.isOfType(AxiomType.EQUIVALENT_CLASSES)) {
+                } else if (axe.isOfType(AxiomType.EQUIVALENT_CLASSES)) {
                     OWLEquivalentClassesAxiom rex = (OWLEquivalentClassesAxiom) axe;
                     Set<OWLSubClassOfAxiom> eqsubclassaxioms = rex.asOWLSubClassOfAxioms();
 
                     for (OWLSubClassOfAxiom subClassAxiom : eqsubclassaxioms) {
                         if (subClassAxiom.getSubClass().isAnonymous() && subClassAxiom.getSuperClass().isAnonymous()) {
                             terminology = false;
+                            break;
                         }
+                        if(!terminology)
+                            break;
                     }
+                    // ignoring the object properties, data properties and ABox
+                } else if (axe.isOfType(AxiomType.CLASS_ASSERTION)
+                        || axe.isOfType(AxiomType.ASYMMETRIC_OBJECT_PROPERTY)
+                        || axe.isOfType(AxiomType.EQUIVALENT_DATA_PROPERTIES)
+                        || axe.isOfType(AxiomType.OBJECT_PROPERTY_ASSERTION)
+                        || axe.isOfType(AxiomType.OBJECT_PROPERTY_DOMAIN)
+                        || axe.isOfType(AxiomType.OBJECT_PROPERTY_RANGE)
+                        || axe.isOfType(AxiomType.SUB_OBJECT_PROPERTY)
+                        || axe.isOfType(AxiomType.SUB_PROPERTY_CHAIN_OF)
+                        || axe.isOfType(AxiomType.INVERSE_OBJECT_PROPERTIES)
+                        || axe.isOfType(AxiomType.SYMMETRIC_OBJECT_PROPERTY)
+                        || axe.isOfType(AxiomType.TRANSITIVE_OBJECT_PROPERTY)
+                        || axe.isOfType(AxiomType.DATA_PROPERTY_ASSERTION)
+                        || axe.isOfType(AxiomType.DATA_PROPERTY_DOMAIN)
+                        || axe.isOfType(AxiomType.DATA_PROPERTY_RANGE)) {
+                    //skip
+                }
+                else {
+                    System.out.println(axe.toString());
+                    System.out.println(axe.getAxiomType().toString());
+                    terminology = false;
+                    break;
                 }
             }
             System.out.print(filePath + ":  ");
             if (terminology) {
                 System.out.println("Terminology");
-            }
-            else {
+            } else {
                 System.out.println("Not a terminology");
             }
 
