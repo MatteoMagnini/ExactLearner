455e2fc7d68c01f95327bdc490f58f263058e23d
Riccardo <riccard.squarcialupi@studio.unibo.it>
2024-03-14 18:12:40 +0100

    feat: integrated workloads in Launch.java

--- src/main/java/org/exactlearner/parser/Launch.java
+++ src/main/java/org/exactlearner/parser/Launch.java
@@ -1,9 +1,12 @@
 package org.exactlearner.parser;
 
-import org.exactlearner.connection.Bridge;
-import org.exactlearner.connection.ChatGPTBridge;
-import org.exactlearner.connection.Gpt4FreeBridge;
-import org.exactlearner.connection.HuggingFaceBridge;
+import org.experiments.Environment;
+import org.experiments.logger.SmartLogger;
+import org.experiments.task.ExperimentTask;
+import org.experiments.task.Task;
+import org.experiments.workload.OllamaModels;
+import org.experiments.workload.OllamaWorkload;
+import org.experiments.workload.OpenAIWorkload;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
 
 import java.io.FileNotFoundException;
@@ -14,7 +17,7 @@ class Launch {
     private final static String FAMILY_ONTOLOGY = "src/main/resources/ontologies/small/family.rdf.owl";
 
     public static void main(String[] args) throws FileNotFoundException {
-        runAnimalOntology();
+        //runAnimalOntology();
         runFamilyOntology();
     }
 
@@ -23,55 +26,56 @@ class Launch {
         var classesNames = parser.getClassesNamesAsString();
         var axiom = parser.getAxioms();
         //askGPT4Free(classesNames);
-        askGPT3(classesNames);
+        //askGPT3(classesNames);
         //askHuggingFace(classesNames);
+        sendChatGPTQuestions(classesNames, new OpenAIWorkload());
     }
 
     private static void runAnimalOntology() {
         var parser = loadOntology(ANIMAL_ONTOLOGY);
         var classesNames = parser.getClassesNamesAsString();
         var axiom = parser.getAxioms();
+        //sendOllamaQuestions(classesNames, new OllamaWorkload());
         //askGPT4Free(classesNames);
-        askGPT3(classesNames);
+        //askGPT3(classesNames);
         //askHuggingFace(classesNames);
     }
 
     private static OWLParser loadOntology(String familyOntology) {
         OWLParser parser = null;
         try {
-            parser = new OWLParserImpl(ANIMAL_ONTOLOGY);
+            parser = new OWLParserImpl(familyOntology);
         } catch (OWLOntologyCreationException e) {
             System.out.println(e.getMessage());
         }
         return parser;
     }
 
-
-    private static void askGPT4Free(Set<String> classesNames) {
-        Gpt4FreeBridge bridge = new Gpt4FreeBridge();
-        sendQuestions(classesNames, "", bridge);
+    private static void sendOllamaQuestions(Set<String> classesNames, OllamaWorkload ollama) {
+        for (String className : classesNames) {
+            for (String className2 : classesNames) {
+                if (!className.equals(className2)) {
+                    String message = "You must reply to this with just one word(True, False): In the real world, is " + className + " a subclass of " + className2 + "?";
+                    ollama.setUp(OllamaModels.MIXTRAL.getModelName(), message);
+                    Task task = new ExperimentTask(message, OllamaModels.MIXTRAL.getModelName(), "Family", message, ollama);
+                    Environment.run(task);
+                    //SmartLogger.isFileInCache(task.getFileName());
+                    //SmartLogger.removeFileFromCache(task.getFileName());
+                }
+            }
+        }
     }
 
-    private static void askGPT3(Set<String> classesNames) {
-        String key = System.getenv("OPENAI_API_KEY");
-        System.out.println(key);
-        ChatGPTBridge bridge = new ChatGPTBridge();
-        sendQuestions(classesNames, "", bridge);
-    }
-
-    private static void askHuggingFace(Set<String> classesNames) {
-        String huggingFaceKey = System.getenv("HUGGINGFACE_API_KEY");
-        HuggingFaceBridge bridge = new HuggingFaceBridge("openai-community/gpt2-xl");
-        sendQuestions(classesNames, huggingFaceKey, bridge);
-    }
-
-    private static void sendQuestions(Set<String> classesNames, String key, Bridge bridge) {
+    private static void sendChatGPTQuestions(Set<String> classesNames, OpenAIWorkload openAI) {
         for (String className : classesNames) {
             for (String className2 : classesNames) {
                 if (!className.equals(className2)) {
                     String message = "Reply to this question with ONLY True or False: In the real world, is " + className + " a subclass of " + className2 + "?";
-                    System.out.println(message);
-                    System.out.println(bridge.ask(message, key));
+                    openAI.setUp(message);
+                    Task task = new ExperimentTask(message, "gpt3.5-turbo", "Family", message, openAI);
+                    Environment.run(task);
+                    SmartLogger.isFileInCache(task.getFileName());
+                    SmartLogger.removeFileFromCache(task.getFileName());
                 }
             }
         }
