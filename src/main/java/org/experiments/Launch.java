67a0137d24a90779906880c7c0f5621697c4463b
Riccardo <riccard.squarcialupi@studio.unibo.it>
2024-03-22 16:24:54 +0100

    feat: extracted Axioms in Manchester Syntax from Ontology, removed unused method.

--- src/main/java/org/experiments/Launch.java
+++ src/main/java/org/experiments/Launch.java
@@ -8,14 +8,22 @@ import org.experiments.task.Task;
 import org.experiments.workload.OllamaModels;
 import org.experiments.workload.OllamaWorkload;
 import org.experiments.workload.OpenAIWorkload;
+import org.semanticweb.owlapi.model.AxiomType;
+import org.semanticweb.owlapi.model.OWLAxiom;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
+import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxOWLObjectRendererImpl;
 
 import java.io.FileNotFoundException;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 class Launch {
-    private final static String ANIMAL_ONTOLOGY = "src/main/resources/ontologies/small/animals.owl";
-    private final static String FAMILY_ONTOLOGY = "src/main/resources/ontologies/small/family.rdf.owl";
+    private final static String ANIMAL = "src/main/resources/ontologies/small/animals.owl";
+    private final static String ANIMAL_INFERRED = "src/main/resources/ontologies/small/animals_inferred.owl";
+    private final static String UNIVERSITY = "src/main/resources/ontologies/small/university.owl";
+    private final static String UNIVERSITY_INFERRED = "src/main/resources/ontologies/small/university_inferred.owl";
+    private final static String GENERATIONS = "src/main/resources/ontologies/small/generations(large).owl";
+    private final static String GENERATIONS_INFERRED = "src/main/resources/ontologies/small/generations_inferred.owl";
 
     private final static String system = "Your sole task is to respond " +
             "to questions that I'm going to ask you with this syntax: 'In the real world," +
@@ -29,58 +37,46 @@ class Launch {
             "You can only respond with either True or False.";
 
     public static void main(String[] args) throws FileNotFoundException {
-        runAnimalOntology();
-        //runFamilyOntology();
+        runOntology();
     }
 
-    private static void runFamilyOntology() {
-        var parser = loadOntology(FAMILY_ONTOLOGY);
+    private static void runOntology() {
+        var parser = loadOntology(GENERATIONS_INFERRED);
         var classesNames = parser.getClassesNamesAsString();
-        var axiom = parser.getAxioms();
-        //askGPT4Free(classesNames);
-        //askGPT3(classesNames);
-        //askHuggingFace(classesNames);
+        var axioms = parser.getAxioms();
+        var filteredManchesterSyntaxAxioms = parseAxioms(axioms);
+
         //sendChatGPTQuestions(classesNames, new OpenAIWorkload());
-        sendOllamaQuestions(classesNames, new OllamaWorkload());
+        sendOllamaQuestions(classesNames, new OllamaWorkload(),"Generations Inferred");
     }
 
-    private static void runAnimalOntology() {
-        var parser = loadOntology(ANIMAL_ONTOLOGY);
-        var classesNames = parser.getClassesNamesAsString();
-        var axiom = parser.getAxioms();
-        sendOllamaQuestions(classesNames, new OllamaWorkload());
-        //askGPT4Free(classesNames);
-        //askGPT3(classesNames);
-        //askHuggingFace(classesNames);
+    private static Set<String> parseAxioms(Set<OWLAxiom> axioms) {
+        return axioms.stream().filter(axiom -> !axiom.isOfType(AxiomType.DECLARATION))
+                .filter(axiom -> !axiom.isOfType(AxiomType.FUNCTIONAL_OBJECT_PROPERTY))
+                .filter(axiom -> !axiom.isOfType(AxiomType.SYMMETRIC_OBJECT_PROPERTY))
+                .filter(axiom -> !axiom.isOfType(AxiomType.CLASS_ASSERTION)).collect(Collectors.toSet())
+                .stream().map(new ManchesterOWLSyntaxOWLObjectRendererImpl()::render).collect(Collectors.toSet());
     }
 
-    private static OWLParser loadOntology(String familyOntology) {
+    private static OWLParser loadOntology(String ontology) {
         OWLParser parser = null;
         try {
-            parser = new OWLParserImpl(familyOntology);
+            parser = new OWLParserImpl(ontology);
         } catch (OWLOntologyCreationException e) {
             System.out.println(e.getMessage());
         }
         return parser;
     }
 
-    private static void sendOllamaQuestions(Set<String> classesNames, OllamaWorkload ollama) {
-        //GET time from system
-        //long start = System.currentTimeMillis();
+    private static void sendOllamaQuestions(Set<String> classesNames, OllamaWorkload ollama, String OntologyName) {
+        SmartLogger.checkCachedFiles();
         for (String className : classesNames) {
             for (String className2 : classesNames) {
                 if (!className.equals(className2)) {
                     String message = "(True or False only, i don't want explanation) In the real world, is " + className + " a subclass of " + className2 + "?";
                     ollama.setUp(OllamaModels.MISTRAL.getModelName(), message, system);
-                    Task task = new ExperimentTask(message, OllamaModels.MISTRAL.getModelName(), "Family", message, system, ollama);
+                    Task task = new ExperimentTask(message, OllamaModels.MISTRAL.getModelName(), OntologyName, message, system, ollama);
                     Environment.run(task);
-                    /*if (System.currentTimeMillis() - start > 120000) {
-                        //close the application and restart it
-                        System.out.println("Time is up, stopping the application");
-                        System.exit(0);
-                    }*/
-                    //SmartLogger.isFileInCache(task.getFileName());
-                    //SmartLogger.removeFileFromCache(task.getFileName());
                 }
             }
         }
