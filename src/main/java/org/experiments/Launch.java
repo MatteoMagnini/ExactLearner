55187e6cd0489957a8d98d40d5eeda24c76f3009
Riccardo <riccard.squarcialupi@studio.unibo.it>
2024-03-28 13:22:04 +0100

    fix: llama2-7b name fixed

--- src/main/java/org/experiments/Launch.java
+++ src/main/java/org/experiments/Launch.java
@@ -6,25 +6,30 @@ import org.exactlearner.parser.OWLParserImpl;
 import org.experiments.logger.SmartLogger;
 import org.experiments.task.ExperimentTask;
 import org.experiments.task.Task;
+import org.experiments.utility.SHA256Hash;
 import org.experiments.workload.OllamaWorkload;
+import org.semanticweb.elk.io.FileUtils;
 import org.semanticweb.elk.util.collections.Pair;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.*;
 import org.yaml.snakeyaml.Yaml;
-import uk.ac.manchester.cs.owl.owlapi.OWLClassImpl;
 import uk.ac.manchester.cs.owl.owlapi.mansyntaxrenderer.ManchesterOWLSyntaxOWLObjectRendererImpl;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Launch {
 
-    private static HashMap<Pair<String,String>,String> queries = new HashMap<>();
+    private static HashMap<Pair<String, String>, String> queries = new HashMap<>();
+
     public static void main(String[] args) {
         // Read the configuration file passed by the user as an argument
         Yaml yaml = new Yaml();
@@ -45,33 +50,33 @@ public class Launch {
             }
         }
 
+
         // Recreate ontologies from results
         //get the classes first
-        if (config.getType().equals("classesQuerying")) {
-            queries.forEach((pair, message) -> {
-                OWLOntologyManager manager= OWLManager.createOWLOntologyManager();
-                try {
-                    OWLOntology rootOntology=manager.loadOntologyFromOntologyDocument(new File(pair.getSecond()));
-                    var reader = new ClassesResultsReader(config.getType(), pair.getFirst(), pair.getSecond(), message, config.getSystem());
-                    reader.computeResults();
-                    var parentClassName = reader.getParentClassName();
-                    var childClassName = reader.getChildClassName();
-                    OWLParserImpl parser = new OWLParserImpl(pair.getSecond());
-                    rootOntology.getSubClassAxiomsForSuperClass(parser.getClasses().get()
-                                    .stream()
-                                    .filter(c -> c.toString().contains(parentClassName))
-                                    .findFirst()
-                                    .get()).stream()
-                            .filter(axiom -> axiom.getSubClass().asOWLClass().toString().contains(childClassName));
-
-                } catch (OWLOntologyCreationException e) {
-                    throw new RuntimeException(e);
-                }
-
-
-
-            });
-        }
+//        if (config.getType().equals("classesQuerying")) {
+//            queries.forEach((pair, message) -> {
+//                OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+//                try {
+//                    OWLOntology rootOntology = manager.loadOntologyFromOntologyDocument(new File(pair.getSecond()));
+//                    var reader = new ClassesResultsReader(config.getType(), pair.getFirst(), pair.getSecond(), message, config.getSystem());
+//                    reader.computeResults();
+//                    var parentClassName = reader.getParentClassName();
+//                    var childClassName = reader.getChildClassName();
+//                    OWLParserImpl parser = new OWLParserImpl(pair.getSecond());
+//                    rootOntology.getSubClassAxiomsForSuperClass(parser.getClasses().get()
+//                                    .stream()
+//                                    .filter(c -> c.toString().contains(parentClassName))
+//                                    .findFirst()
+//                                    .get()).stream()
+//                            .filter(axiom -> axiom.getSubClass().asOWLClass().toString().contains(childClassName));
+//
+//                } catch (OWLOntologyCreationException e) {
+//                    throw new RuntimeException(e);
+//                }
+//
+//
+//            });
+//        }
     }
 
     private static void runExperiment(String model, String ontology, String system, int maxTokens, String type) {
@@ -80,29 +85,40 @@ public class Launch {
         var axioms = parser.getAxioms();
         var filteredManchesterSyntaxAxioms = parseAxioms(axioms);
 
-        if (type.equals("classesQuerying")){
+        if (type.equals("classesQuerying")) {
             for (String className : classesNames) {
                 for (String className2 : classesNames) {
                     if (!className.equals(className2)) {
                         String message = className + " SubClassOf " + className2;
-                        queries.put(new Pair<>(model, ontology), message);
-                        var work = new OllamaWorkload(model, system, message, maxTokens);
-                        Task task = new ExperimentTask(type, model, ontology, message, system, work);
-                        Environment.run(task);
+                        //queries.put(new Pair<>(model, ontology), message);
+                        runModel(model, ontology, system, maxTokens, type, message);
                     }
                 }
             }
-        }else if (type.equals("axiomsQuerying")) {
+        } else if (type.equals("axiomsQuerying")) {
             for (String axiom : filteredManchesterSyntaxAxioms) {
                 // Remove carriage return and line feed characters
                 axiom = axiom.replaceAll("\r", " ").replaceAll("\n", " ");
-                var work = new OllamaWorkload(model, system, axiom, maxTokens);
-                Task task = new ExperimentTask(type, model, ontology, axiom, system, work);
-                Environment.run(task);
+                runModel(model, ontology, system, maxTokens, type, axiom);
             }
-        }else{
+        } else {
             throw new IllegalStateException("Invalid type of experiment.");
         }
+    }
+
+    private static void runModel(String model, String ontology, String system, int maxTokens, String type, String message) {
+        var work = new OllamaWorkload(model, system, message, maxTokens);
+        Task task = new ExperimentTask(type, model, ontology, message, system, work);
+        Environment.run(task);
+        moveFile(type, model, ontology, message, system);
+    }
+
+    private static void moveFile(String type, String model, String ontology, String message, String system) {
+        File f = new File("cache/" + SHA256Hash.sha256(type + model + ontology + message + system) + ".csv");
+        var ontName = ontology.split("/")[5].replace(".owl", "");
+        if (!new File("cache/"+type+"-"+model+"-"+ontName).exists())
+            new File("cache/"+type+"-"+model+"-"+ontName).mkdir();
+        f.renameTo(new File("cache/"+type+"-"+model+"-"+ontName+"/"+f.getName()));
     }
 
     private static Set<String> parseAxioms(Set<OWLAxiom> axioms) {
