971e05394edd2ffb3bc71fe854f2254e2b75964f
Riccardo <riccard.squarcialupi@studio.unibo.it>
2024-04-15 17:14:32 +0200

    fix: fixed statements with repeated classes names.

--- src/main/java/org/pac/StatementBuilderImpl.java
+++ src/main/java/org/pac/StatementBuilderImpl.java
@@ -25,23 +25,17 @@ public class StatementBuilderImpl implements StatementBuilder {
     // Type 1: (A ∩ B) ⊑ C
     private void createStatementsType1() {
         classes.forEach(c1 -> {
-            classes.forEach(c2 -> {
-                classes.stream()
-                        .filter(c3 -> !c1.equals(c3) && !c2.equals(c3))
-                        .forEach(c3 -> {
-                            generatedStatementsType1.add("( " + c1 + " and " + c2 + " ) SubClassOf: " + c3);
-                        });
-            });
+            classes.stream().filter(c2 -> !c2.equals(c1)).forEach(c2 -> classes.stream()
+                    .filter(c3 -> !c3.equals(c1) && !c3.equals(c2))
+                    .forEach(c3 -> generatedStatementsType1.add("( " + c1 + " and " + c2 + " ) SubClassOf: " + c3)));
         });
     }
 
     // Type 2: B ⊑ ∃R.A
     private void createStatementsType2() {
         classes.forEach(c1 -> {
-            classes.stream().filter(c2 -> !c1.equals(c2)).forEach(c2 -> {
-                objectProperties.forEach(o -> {
-                    generatedStatementsType2.add(c1 + " SubClassOf: ( " + o + " some " + c2 + " )");
-                });
+            classes.stream().filter(c2 -> !c2.equals(c1)).forEach(c2 -> {
+                objectProperties.forEach(o -> generatedStatementsType2.add(c1 + " SubClassOf: ( " + o + " some " + c2 + " )"));
             });
         });
     }
@@ -49,10 +43,8 @@ public class StatementBuilderImpl implements StatementBuilder {
     // Type 3: ∃R.A ⊑ B
     private void createStatementsType3() {
         classes.forEach(c1 -> {
-            classes.stream().filter(c2 -> !c1.equals(c2)).forEach(c2 -> {
-                objectProperties.forEach(o -> {
-                    generatedStatementsType3.add("( " + o + " some " + c1 + " ) SubClassOf: " + c2);
-                });
+            classes.stream().filter(c2 -> !c2.equals(c1)).forEach(c2 -> {
+                objectProperties.forEach(o -> generatedStatementsType3.add("( " + o + " some " + c1 + " ) SubClassOf: " + c2));
             });
         });
     }
@@ -68,4 +60,14 @@ public class StatementBuilderImpl implements StatementBuilder {
         int randomIndex = (int) (Math.random() * allStatements.size());
         return (String) allStatements.toArray()[randomIndex];
     }
+
+    @Override
+    public Integer getNumberOfStatements() {
+        Set<String> allStatements = new HashSet<>();
+        allStatements.addAll(generatedStatementsType1);
+        allStatements.addAll(generatedStatementsType2);
+        allStatements.addAll(generatedStatementsType3);
+        return allStatements.size();
+    }
+
 }
