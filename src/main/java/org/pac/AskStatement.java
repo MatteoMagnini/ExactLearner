10cb9514a68b28392ce7d206f24e713f9c9c06ef
Riccardo <riccard.squarcialupi@studio.unibo.it>
2024-04-17 15:29:26 +0200

    feat: added statement cached response from the LLMs models.

--- src/main/java/org/pac/AskStatement.java
+++ src/main/java/org/pac/AskStatement.java
@@ -1,14 +1,17 @@
 package org.pac;
 
+import org.analysis.OntologyManipulator;
 import org.experiments.Configuration;
 import org.experiments.Environment;
+import org.experiments.logger.SmartLogger;
 import org.experiments.task.ExperimentTask;
 import org.experiments.task.Task;
-import org.experiments.utility.OntologyLoader;
 import org.experiments.utility.YAMLConfigLoader;
 import org.experiments.workload.OllamaWorkload;
 import org.experiments.workload.OpenAIWorkload;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.stream.Collectors;
 
 public class AskStatement {
@@ -19,21 +22,27 @@ public class AskStatement {
 
     public AskStatement(String YMLFilePath) {
         var config = new YAMLConfigLoader().getConfig(YMLFilePath, Configuration.class);
-
+        SmartLogger.checkCachedFiles();
+        var cpus = Runtime.getRuntime().availableProcessors();//THIS COUNT LOGIC CORES NOT PHYSICAL CORES
+        ExecutorService executorService = Executors.newFixedThreadPool(cpus/2-1);
         for (String model : config.getModels()) {
-            for (String ontology : config.getOntologies()) {
-                System.out.println("Asking statements for model: " + model + " and ontology: " + ontology);
-                askStatement(model, ontology, config.getSystem(), config.getMaxTokens(), config.getType());
-            }
+            executorService.execute(() -> {
+                for (String ontology : config.getOntologies()) {
+                    executorService.execute(() -> {
+                        System.out.println("Asking statements for model: " + model + " and ontology: " + ontology);
+                        askStatement(model, ontology, config.getSystem(), config.getMaxTokens(), config.getType());
+                    });
+                }
+            });
         }
     }
 
     private void askStatement(String model, String ontology, String system, int maxTokens, String type) {
-        var parser = new OntologyLoader().getParser(ontology);
+        var parser = OntologyManipulator.getParser(ontology);
         var builder = new StatementBuilderImpl(parser.getClassesNamesAsString(), parser.getObjectProperties().stream().map(Object::toString).map(s -> s.split("#")[1].replace(">", "")).collect(Collectors.toSet()));
 
         builder.getAllStatements().forEach(s -> {
-            Runnable work = null;
+            Runnable work;
             if (OllamaWorkload.supportedModels.contains(model)) {
                 work = new OllamaWorkload(model, system, s, maxTokens);
             } else if (OpenAIWorkload.supportedModels.contains(model)) {
